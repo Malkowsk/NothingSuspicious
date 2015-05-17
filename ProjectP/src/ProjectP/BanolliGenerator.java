176b0b1eb4750f79863d11a5a684ceacccdf2362
malkowsk <malkowsk@gmail.com>
2015-05-17 11:05:39 +0200

    Minor crap

--- ProjectP/src/ProjectP/BanolliGenerator.java
+++ ProjectP/src/ProjectP/BanolliGenerator.java
@@ -1,7 +1,11 @@
 package ProjectP;
 
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 // Poject P - Jesus
 
@@ -16,16 +20,52 @@ public class BanolliGenerator {
 	
 	}
 	
-
-	public static void main(String[] args) {
-		Pizza p1 = returnPizza();
-		Pizza p2 = null;
-		do {
-			p2 = returnPizza();
-		} while(p1==p2);
-		Pizza[] pizzas = { p1, p2 };
-		System.out.println("Todays menu: " + pizzas[0] + " and " + pizzas[1]);
-
+	public static ArrayList<Pizza> withDuplicates(int quantity) {
+		ArrayList<Pizza> pizzas = new ArrayList<>(quantity);			
+		for(int i = 0; i<quantity; i++) 
+			pizzas.add(returnPizza());
+		return pizzas;
 	}
 	
+	public static Set<Pizza> withoutDuplicates(int quantity) {
+		Set<Pizza> pizzas = new HashSet<>(quantity);
+		while(pizzas.size()<quantity) {
+			pizzas.add(returnPizza());
+		}
+		return pizzas;
+	}
+	
+
+	public static void getPizzaList(int quantity, boolean allowDuplicates) {
+		
+		Collection<Pizza> pizzas;
+		
+		if(allowDuplicates) 
+			pizzas = withDuplicates(quantity);
+		else
+			pizzas = withoutDuplicates(quantity);
+		
+		System.out.println("Todays menu :");
+		
+		for(Pizza p : pizzas) {
+			System.out.println(p);
+		}
+			
+	}
+	
+	public static void main(String[] args) {
+		getPizzaList(5, true);
+		System.out.println();
+		getPizzaList(7, false);
+	}
+		
+		//Pizza p1 = returnPizza();
+		//Pizza p2 = null;
+		//do {
+			//p2 = returnPizza();
+		//} while(p1==p2);
+		//Pizza[] pizzas = { p1, p2 };
+		//System.out.println("Todays menu: " + pizzas[0] + " and " + pizzas[1]);
+
 }
+	
